294e95e0cbe813800fef59380455276e9f403887
JDCG <jcarreterogranado.guadalupe@alumnado.fundacionloyola.net>
2021-11-16 23:16:09 +0100

    v1

---
+++ 1Cookies/borrar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies</title>
+</head>
+<body>
+    <h1>Cookies</h1>
+    <div>
+        <a href="inicio.php">Inicio(Crear Cookie)</a>&nbsp;&nbsp;&nbsp;
+        <a href="seguir.php">Seguir navegando</a>
+    </div>
+</body>
+</html>
+<?php
+    if(isset($_COOKIE['usuario'])){
+        setcookie("usuario", "Juan Diego", time()-60);
+    }
+?>
